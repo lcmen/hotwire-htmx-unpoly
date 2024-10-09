d0c4c8bbebee201218b8efaa81d9fa29fb038590
Lucas Mendelowski <lucas@mendelowski.com>
2024-10-09 22:57:01 +0200

    Finish animations for Turbo

--- app/assets/javascript/controllers/modal_controller.js
+++ app/assets/javascript/controllers/modal_controller.js
@@ -7,21 +7,39 @@ export default class extends Controller {
 
   connect() {
     if (this.openValue) this.open()
+    this.boundBeforeRender = this.beforeRender.bind(this)
+    document.addEventListener("turbo:before-render", this.boundBeforeRender)
   }
 
   disconnect() {
-    document.removeEventListener("turbo:before-cache", this.boundBeforeCache)
+    document.removeEventListener("turbo:before-render", this.boundBeforeRender)
   }
 
   backdropClose(event) {
     if (event.target.nodeName == "DIALOG") this.close()
   }
 
+  beforeRender(event) {
+    event.preventDefault()
+
+    this.close().then(() => event.detail.resume())
+  }
+
   open() {
     this.element.showModal()
   }
 
-  close() {
-    this.element.close()
+  close(event) {
+    if (event && event.detail && typeof event.detail === 'object' && !event.detail.success) return
+    if (event) event.preventDefault()
+
+    this.element.setAttribute("closing", "")
+
+    return Promise.all(
+      this.element.getAnimations().map((animation) => animation.finished),
+    ).then(() => {
+      this.element.removeAttribute("closing")
+      this.element.close()
+    })
   }
 }
--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -9,8 +9,25 @@ module.exports = {
   ],
   theme: {
     extend: {
+      animation: {
+        'fade-in': 'fade-in 0.5s forwards',
+        'fade-out': 'fade-out 0.5s forwards',
+      },
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+      boxShadow: {
+        '100vw': '0 0 0 100vw rgba(0, 0, 0, 0.5)',
+      },
+      keyframes: {
+        'fade-in': {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        },
+        'fade-out': {
+          '0%': { opacity: '1' },
+          '100%': { opacity: '0' },
+        },
       },
     },
   },
