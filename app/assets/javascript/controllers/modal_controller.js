164a3d04c66b0810073964f7e877c1fc4adb118a
Lucas Mendelowski <lucas@mendelowski.com>
2024-10-15 22:11:52 +0200

    Simplify Hotwire code

--- app/assets/javascript/controllers/modal_controller.js
+++ app/assets/javascript/controllers/modal_controller.js
@@ -7,34 +7,14 @@ export default class extends Controller {
 
   connect() {
     if (this.openValue) this.open()
-    this.boundBeforeCache = this.beforeCache.bind(this)
-    this.boundBeforeRender = this.beforeRender.bind(this)
-
-    // document.addEventListener("turbo:before-cache", this.boundBeforeCache)
-    // document.addEventListener("turbo:before-render", this.boundBeforeRender)
-  }
-
-  disconnect() {
-    // document.removeEventListener("turbo:before-cache", this.boundBeforeCache)
-    // document.removeEventListener("turbo:before-render", this.boundBeforeRender)
   }
 
   backdropClose(event) {
     if (event.target.nodeName == "DIALOG") this.close()
   }
 
-  beforeCache() {
-    this.close()
-  }
-
-  beforeRender(event) {
+  closeWithAnimation(event) {
     if (this.openValue) this.close(event).then(() => event.detail.resume())
-  }
-
-  submit(event) {
-    event.detail.formSubmission.stop();
-    event.preventDefault();
-    return this.close()
   }
 
   open() {
@@ -43,7 +23,6 @@ export default class extends Controller {
   }
 
   close(event) {
-    // Don't follow the link if the modal is closing by clicking the cancel button
     if (event) event.preventDefault()
 
     this.element.setAttribute("closing", "")
