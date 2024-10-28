495f77323fe5713980cef48ae0925dda514594ea
Lucas Mendelowski <lucas@mendelowski.com>
2024-10-28 19:14:36 +0100

    Fix unpoly modals

--- app/assets/javascript/unpoly-config.js
+++ app/assets/javascript/unpoly-config.js
@@ -3,4 +3,12 @@ document.addEventListener('DOMContentLoaded', function() {
   up.layer.config.modal.closeAnimation = 'fade-out'
   up.layer.config.modal.openDuration = 500
   up.layer.config.modal.closeDuration = 500
+
+  up.on('up:layer:dismiss', (event) => {
+    console.log('up:layer:dismiss', event);
+  });
+
+  up.on('up:layer:accept', (event) => {
+    console.log('up:layer:accept', event);
+  });
 });
