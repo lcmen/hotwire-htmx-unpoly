9b0d675145054f4ee4814e077c939d0ccdce320d
Lucas Mendelowski <lucas@mendelowski.com>
2024-11-27 22:06:17 +0100

    Remove unnecessary `hx-get` for HTMX links

--- app/assets/javascript/controllers/modal_controller.js
+++ app/assets/javascript/controllers/modal_controller.js
@@ -1,13 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static values = {
-    open: Boolean
-  }
-
-  connect() {
-    if (this.openValue) this.open()
-  }
+  static values = { open: Boolean }
 
   backdropClose(event) {
     if (event.target.nodeName == "DIALOG") this.close()
