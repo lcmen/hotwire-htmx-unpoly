2c9726077aca66f52d1f591003dd60249ac151c1
Lucas Mendelowski <lucas@mendelowski.com>
2024-12-25 08:54:47 +0100

    Update template

--- app/views/tasks/edit.inertia.js
+++ app/views/tasks/edit.inertia.js
@@ -3,9 +3,11 @@ import TaskForm from 'views/tasks/_form.inertia';
 
 const TasksEdit = ({ task }) => {
   return html`
-    <div data-mode="modal">
-      <h1 class="font-bold text-4xl">Edit task</h1>
-      <${TaskForm} task=${task} />
+    <div class="mx-auto md:w-2/3 w-full">
+      <div>
+        <h1 class="font-bold text-4xl">Edit task</h1>
+        <${TaskForm} task=${task} />
+      </div>
     </div>
   `
 }
