bb01de7b652448ecb623d971ac9fb130562fb9de
Lucas Mendelowski <lucas@mendelowski.com>
2024-12-26 17:09:34 +0100

    Add modal animation

--- app/assets/javascript/screens/index.js
+++ app/assets/javascript/screens/index.js
@@ -1,6 +1,6 @@
 import { html } from 'utils'
 import { usePage } from '@inertiajs/react'
-import { Fragment, createElement } from 'react'
+import { Fragment, createElement, useEffect, useRef, useState } from 'react'
 
 import TasksIndex from 'views/tasks/index.inertia';
 import TasksNew from 'views/tasks/new.inertia';
@@ -13,17 +13,35 @@ const screens = {
 }
 
 const Modal = ({ component, props }) => {
-  const screen = screens[component]
+  const [closing, setClosing] = useState(false)
+  const [open, setOpen] = useState(false)
+  const [screen, setScreen] = useState(null)
+  const ref = useRef(null)
+
+  useEffect(() => {
+    if (component) {
+      setOpen(true)
+      setScreen(createElement(screens[component], props))
+    } else if (open && !closing) {
+      setClosing(true)
+    } else if (open && closing) {
+      Promise.all(ref.current.getAnimations().map(animation => animation.finished)).then(() => {
+        setScreen(null)
+        setOpen(false)
+        setClosing(false)
+      })
+    }
+  }, [component, closing, open, setClosing, setOpen, setScreen])
 
   return html`
     <dialog
       id="modal"
-      aria-modal="true"
       class="modal"
-      open="true"
-      role="dialog"
+      open=${open}
+      closing=${closing ? 'true' : null}
+      ref=${ref}
     >
-      ${createElement(screen, props)}
+      ${screen ? screen : null}
     </dialog>
   `
 }
@@ -39,7 +57,7 @@ const Layout = ({ children }) => {
         ${children}
       </div>
 
-      ${modal && html`<${Modal} component=${modal.component} props=${modal.props} />`}
+      <${Modal} component=${modal?.component} props=${modal?.props} />
     <//>
   `
 }
